d39367eebbaad4ad60ce0aeef08c9a7772c56f5d
Lufrut <superkolasar5@gmail.com>
2023-08-20 16:44:49 +0200

    I hope it'll work

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -7,9 +7,11 @@ public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
         Predicate<String> check = str -> {
             if(str.contains("-")) return false;
-            if(str.contains("0")) return false;
-            if(str.isEmpty()) return false;
-            return !str.isBlank();
+            else if(str.contains("0")) return false;
+            else if(str.isEmpty()) return false;
+            else if(str.isBlank()) return false;
+            else if(str == null) return false;
+            else return true;
         };
         return args.stream()
                 .allMatch(check);
